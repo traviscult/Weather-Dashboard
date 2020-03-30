126afd5c52ab3e6ce8e529a2fc364feccfb32f7e
traviscult <traviscult@gmail.com>
2020-03-29 21:04:44 -0400

    added get UV index functionality

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ $(document).ready(function () {
         currentLocationForecast(searchValue);
         fiveDayForecast(searchValue);
         // history (searchVaule);
-        buildQueryURL(searchValue)
+        buildQueryURL(searchValue);
     });
 
     
     function buildQueryURL(searchValue) {
 
         let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchValue + '&appid=c44d280310ef4a863369e49f9f7d2dba'
-
+    
         $.ajax({
             url: queryURL,
             method: 'GET'
@@ -25,11 +25,13 @@ $(document).ready(function () {
            .then(function(response){
             console.log(response);
 
-            let card = $('<div>').addClass("card")
-            let city = $('<h1>').addClass("card-title").text(response.name + " (" + new Date().toLocaleDateString() + ")")
-            let temp = $('<p>').addClass("card-text").text("temperature: " + ((response.main.temp - 273.15) * 9/5 +32).toFixed(2))
-            let humidity = $('<p>').addClass("card-text").text("Humidity: " + response.main.humidity)
-            let windSpeed = $('<p>').addClass("card-text").text("Wind-Speed: " + response.wind.speed)
+            let iconURL = "<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'>"
+
+            let card = $('<div>').addClass("card");
+            let city = $('<h1>').addClass("card-title").text(response.name + " (" + new Date().toLocaleDateString() + ")").append(iconURL);
+            let temp = $('<p>').addClass("card-text").text("temperature: " + ((response.main.temp - 273.15) * 9/5 +32).toFixed(2));
+            let humidity = $('<p>').addClass("card-text").text("Humidity: " + response.main.humidity);
+            let windSpeed = $('<p>').addClass("card-text").text("Wind-Speed: " + response.wind.speed);
             // let uvIndex = $('<p>').addClass("card-text").text(response.)
 
             let cardBody = $('<div>').addClass("card-body");
@@ -37,16 +39,50 @@ $(document).ready(function () {
             card.append(cardBody);
 
             $('#today').append(card)
-        
+            
+            fiveDayForecast(searchValue)
+            getUvIndex(response.coord.lat, response.coord.lon)
            })
 
     }
 
-    function getUvIndex(){
+    function getUvIndex(lat, lon){
 
-        let uvQuery 
+        let uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=c44d280310ef4a863369e49f9f7d2dba&lat="+  lat + "&lon=" + lon;
         
+        $.ajax({
+            url: uvQueryURL,
+            method:'GET'
+        })
+        .then(function(response){
+
+            let uvIndex = $('<p>').addClass("card-text").text("UV Index: " + response.value)
+
+            let card = $('<div>').addClass("card");
+            let cardBody = $('<div>').addClass("card-body");
+            cardBody.append(uvIndex);
+            card.append(cardBody);
+
+            $('#today').append(card)
+        })
     }
+
+
+    function fiveDayForecast(searchValue){
+        let fiveDayURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchValue + '&appid=c44d280310ef4a863369e49f9f7d2dba'
+
+        $.ajax({
+            url: fiveDayURL,
+            method: 'GET'
+           })
+           .then(function(response){
+            console.log(response);
+        
+        
+        })
+
+    }
+
 
    function searchWeather(){
 
@@ -55,14 +91,10 @@ $(document).ready(function () {
 
    function currentLocationForecast (searchValue){
       
-
-
    }
 
 
-    function fiveDayForecast(){
-
-    }
+    
 
   function uvIndex () {
 
